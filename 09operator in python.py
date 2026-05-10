51c121524caa26ff1ce2a5a795578f6c2eb2c037
justin <justinmendonsa2004@gmail.com>
2026-05-10 21:08:04 +0530

    code1

---
+++ 09operator in python.py
@@ -0,0 +1,73 @@
+"""
+1.Arithmetic Operators....
+
+| Operator | Meaning             | Example  | Output |
+| -------- | ------------------- | -------- | ------ |
+| `+`      | Addition            | `5 + 2`  | `7`    |
+| `-`      | Subtraction         | `5 - 2`  | `3`    |
+| `*`      | Multiplication      | `5 * 2`  | `10`   |
+| `/`      | Division            | `5 / 2`  | `2.5`  |
+| `//`     | Floor Division      | `5 // 2` | `2`    |
+| `%`      | Modulus (Remainder) | `5 % 2`  | `1`    |
+| `**`     | Power               | `5 ** 2` | `25`   |
+
+
+2.Comparison Operators...
+
+| Operator | Meaning               | Example  |
+| -------- | --------------------- | -------- |
+| `==`     | Equal to              | `5 == 5` |
+| `!=`     | Not equal to          | `5 != 3` |
+| `>`      | Greater than          | `5 > 3`  |
+| `<`      | Less than             | `5 < 3`  |
+| `>=`     | Greater than or equal | `5 >= 5` |
+| `<=`     | Less than or equal    | `5 <= 3` |
+
+
+3. Assignment Operators
+
+| Operator | Example  | Same As      |
+| -------- | -------- | ------------ |
+| `=`      | `x = 5`  | Assign value |
+| `+=`     | `x += 2` | `x = x + 2`  |
+| `-=`     | `x -= 2` | `x = x - 2`  |
+| `*=`     | `x *= 2` | `x = x * 2`  |
+| `/=`     | `x /= 2` | `x = x / 2`  |
+
+
+
+4. Logical Operators...
+
+| Operator | Meaning               |
+| -------- | --------------------- |
+| `and`    | True if both are true |
+| `or`     | True if one is true   |
+| `not`    | Reverse condition     |
+
+
+5. Identity Operators...
+| Operator | Meaning          |
+| -------- | ---------------- |
+| `is`     | Same object      |
+| `is not` | Different object |
+
+
+6. Membership Operators....
+| Operator | Meaning     |
+| -------- | ----------- |
+| `in`     | Present     |
+| `not in` | Not present |
+
+
+7. Bitwise Operators....
+| Operator | Meaning     |    |
+| -------- | ----------- | -- |
+| `&`      | AND         |    |
+| `        | `           | OR |
+| `^`      | XOR         |    |
+| `~`      | NOT         |    |
+| `<<`     | Left Shift  |    |
+| `>>`     | Right Shift |    |
+
+
+"""
---
+++ 13practical calculator.py
@@ -0,0 +1,21 @@
+print("Press 1 for Addition")
+print("Press 1 for Subtraction")
+print("Press 1 for Multiplication")
+print("Press 1 for Division")
+print("=========================")
+
+choice = int(input("Enter your choice \n"))
+num1 = int(input("Enter your first number \n"))
+num2 = int(input("Enter your Second number \n"))
+
+
+if choice == 1:
+    print("addition of two number is = ", num1 + num2)
+elif choice == 2:
+    print("Subtraction of two number is = ", num1 - num2)
+elif choice == 3:
+    print("Multiplication of two number is = ", num1 * num2)
+elif choice == 4:
+    print("Division of two number is = ", num1 / num2)
+else:
+    print("Invalid option")
