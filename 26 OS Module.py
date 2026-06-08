4e284f6a5231f4d1d8ce2c5637e463bcb5c69bc4
justin <justinmendonsa2004@gmail.com>
2026-06-08 17:36:30 +0530

    update

---
+++ 26 OS Module.py
@@ -0,0 +1,8 @@
+import os
+
+#n = os.getcwd()
+#n = os.listdir()
+#os.mkdir("script") //it helps create dir
+#os.rename('23f-string in .py','23-string in.py') #it helps to rename the file name.
+n = os.path.isfile('23-string in.py')
+#print(n)
---
+++ 27 dice game using .py
@@ -0,0 +1,28 @@
+import random
+
+def roll():
+    lis = [1, 2, 3, 4, 5, 6]
+    num =random.choice(lis)
+    print(num)
+    
+while True:   
+    
+    print("Do you want to play the Game")
+    choice = input("Yes/No")
+    if choice in "yes":
+       print("How many players")
+       nop = int(input("In numbers"))
+       while True:
+           for players in range(1, nop+1):
+               print("Do you want to roll the dice")
+               pc = int(input("Enter 1 to roll 0 to exit"))
+               if pc == 1:
+                   roll()
+               elif pc == 0:
+                   break
+               else:
+                   print("invalid chioce") 
+    elif choice in "no":
+        break
+    else:
+        print("invalid choice")
