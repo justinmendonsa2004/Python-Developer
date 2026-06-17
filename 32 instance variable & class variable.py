475fe97408fe28a4ea4e747b3cf78fb24e337d01
justin <justinmendonsa2004@gmail.com>
2026-06-17 23:44:46 +0530

    code2

--- 32 instance variable & class variable.py
+++ 32 instance variable & class variable.py
@@ -6,7 +6,7 @@ class Class1:
 #print(Class1.var1)#class variables
 
 ins1 = Class1()
-ins2 = Class1()#instance variables
+ins2 = Class1()#instance variables0
 ins1.var1 ="script kiddie"
 print(ins1.var1)
 print(ins2.var1)
